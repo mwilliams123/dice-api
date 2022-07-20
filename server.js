ddf2b96e6edeb66a4516a1410b18617243d50d1e
Megan Williams <wilm140@ucla.edu>
2022-07-19 23:23:31 -0600

    Implement API

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ app.get('/', (req, resp) => {
     resp.sendFile(__dirname + '/index.html')
 })
 
+/*
+Return a random number between 1 and nsides.
+*/
 app.get('/api/:nsides', (req, resp) => {
-    console.log(req.params.nsides)
-    let n = Number(req.params.nsides) 
-    const data = {'roll' : n}
+    const n = Number(req.params.nsides)
+    if (!n || n < 1) {
+        resp.status(500).send('Parameter must be valid number greater than 0.')
+        return
+    }
+    const result = Math.floor(Math.random() * n) + 1;
+    const data = {'roll' : result}
     resp.json(data)
 })
 
